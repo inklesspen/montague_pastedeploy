4221c41a8f27f18a8d660777318b89577209c8ea
Jon Rosebaugh <jon@inklesspen.com>
2015-06-14 11:49:46 -0400

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*names, **kwargs):
 
 setup(
     name="montague_pastedeploy",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     description=("Montague provides functions to load WSGI apps and servers "
                  "based on configuration files. "
